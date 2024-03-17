84d367ee6ebc735405cab9eddc40912662779581
iLLuMiiNaTy <rieuvernetnicolas@gmail.com>
2024-03-17 22:05:54 +0100

    Blogs fonctionnels

---
+++ src/app/interface/blog-details.ts
@@ -0,0 +1,10 @@
+export interface BlogDetails {
+    id: number;
+    overview: string;
+    poster_path: string;
+    release_date: string;
+    title: string;
+    vote_count: number;
+    newUrl: boolean;
+    author: string;
+  }
