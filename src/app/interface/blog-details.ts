5938abefa12906f166504d9f51418cdcfa34255a
iLLuMiiNaTy <rieuvernetnicolas@gmail.com>
2024-03-18 18:59:12 +0100

    ajout visibilité des blogs

--- src/app/interface/blog-details.ts
+++ src/app/interface/blog-details.ts
@@ -7,4 +7,7 @@ export interface BlogDetails {
     vote_count: number;
     newUrl: boolean;
     author: string;
+    visibility: string;
+    // Liste des utilisateurs ayant accès au blog restreint
+    restrictedUsers: string[];
   }
